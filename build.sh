07d0e19b36c1db2cb651533e9dfd740ab0cda0bc
Aivan <AivanF@users.noreply.github.com>
2019-09-22 01:10:16 +0300

    Improved building

--- build.sh
+++ build.sh
@@ -1,16 +1,36 @@
 
 echo "----- Building Lemuras JS -----"
 
+debug=0
+
+while test $# -gt 0; do
+case "$1" in
+	-D|--debug)
+		debug=1
+		shift
+		;;
+	*)
+		echo "Unknown arg" $1
+		shift
+		;;
+	esac
+done
+
 printf "Lines of code: "
 find ./lemuras "(" -name "*.js" ")" -print0 | xargs -0 wc -l | tail -1 | awk '{print $1;}'
 
 # Combine files
 ncc build ./lemuras/init.js -o dist
 
-# Minify it
-uglifyjs ./dist/index.js >> ./dist/min.js
-# Debugging way
-# cat ./dist/index.js >> ./dist/min.js
+if [ $debug -eq 0 ]; then
+	echo "Prod version"
+	# Minify it
+	uglifyjs ./dist/index.js > ./dist/min.js
+else
+	echo "Debug version"
+	# Put as is
+	cat ./dist/index.js > ./dist/min.js
+fi
 
 # Wrap as a module
 cat ./other/header.js > ./lemuras.js
